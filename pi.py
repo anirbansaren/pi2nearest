911a494410b8f8c000f6045b8731f440ad470c10
Anirban Saren <anirbansaren@gmail.com>
2025-01-26 21:50:20 +0530

    Find pi to nth digit using round() function

--- pi.py
+++ pi.py
@@ -1,3 +1,5 @@
+from math import pi
+
 def input_n():
 # this function takes input from the user
     while True:
@@ -12,5 +14,25 @@ def input_n():
     # this returns the value of the integer input
     return num
 
-print("The inputted number is:\n{}".format(input_n()))
+def calculate_pi(n):
+    # this function will calculate the value of pi to the nth digit
+    pi_value = pi
+    pi_n = round(pi_value, n)
+    print("The value of pi to the {a}th digit after decimal points is:\n{b}".format(a=n, b=pi_n))
 
+def welcome():
+    # this function welcomes the user and starts the program
+    print("         WELCOME TO THIS pi2nearest APP!!!       ")
+    print("         Brought to you by Anirban Saren         ")
+    print("\n\n\n")
+    print("Instructions:")
+    print("1. We will ask you to enter the value of n (the number of digits in pi after the decimal point)")
+    print("2. Then we will print out the value of pi till that number of digits after decimal.")
+    print("\n\n\n")
+    print("         LET'S GOOOOOO!!!!!!!!                   ")
+    
+    value_of_n = input_n()
+    calculate_pi(value_of_n)
+
+if __name__=="__main__":
+    welcome()
